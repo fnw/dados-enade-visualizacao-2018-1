455d413c7974a8d5c33de49f395401f7f9bae75d
Felipe Nunes Walmsley <fnw@cin.ufpe.br>
2018-07-03 13:43:08 -0300

    Reorganizando em pastas, usando CDN no que possível.

---
+++ project_js/div.js
@@ -0,0 +1,45 @@
+function fillTextDiv(vis)
+{
+	var h2Element = document.querySelector('h2#header-explanation')
+	var pElement = document.querySelector('p#explanation-text')
+
+	let h2Text = null
+	let pText = null
+
+	if(vis == 'vis1-mean')
+	{
+		h2Text = 'Average grade by state'
+		pText = 'Higher values indicate better average performance by the students.'
+	}
+	else if(vis == 'vis1-top')
+	{
+		h2Text = 'Relative percentage of top scoring students'
+		pText = 'Values higher than 1 indicate that the state had a higher percentage of students in the top one percent than expected given the number of students from the state (good). '
+		pText += 'Values lower than 1 indicate the oposite, which means the state performed poorly.'
+	}
+	else if(vis == 'vis1-bottom')
+	{
+		h2Text = 'Relative percentage of bottom scoring students'
+		pText = 'Values higher than 1 indicate that the state had a higher percentage of students in the bottom one percent than expected given the number of students from the state, which is bad. '
+		pText += 'Values lower than 1 indicate the oposite, which means the state performed better than expected.'
+	}
+	if(vis == 'vis2-mean')
+	{
+		h2Text = 'Gini coefficient of average grades'
+		pText = 'Values close to 0 indicate that all institutions in the state performed about the same, while values close to 1 indicate that the average grades are very unequal.'
+	}
+	else if(vis == 'vis2-top')
+	{
+		h2Text = 'Gini coefficient of relative percentage of top scoring students'
+		pText = 'Values close to 0 indicate that all institutions have about the same number of top performing students, while values close to 1 indicate that a few institutions concentrate the top performers.'
+	}
+	else if(vis == 'vis2-bottom')
+	{
+		h2Text = 'Gini coefficient of relative percentage of bottom scoring students'
+		pText = 'Values close to 0 indicate that all institutions have about the same number of low scoring students, while values close to 1 indicate that a few institutions have the worst students.'
+	}
+
+	h2Element.innerHTML = h2Text
+	pElement.innerHTML = pText
+
+}
---
+++ project_js/main.js
@@ -0,0 +1,109 @@
+//Variáveis globais do mapa do Leaflet
+
+var myMap = null;
+var div = d3.select("#leaflet-container").append("div").attr("id","leafletmap");
+var geoLayer = null;
+var JSONData = null;
+var popup = L.popup()
+
+//Variáveis de estado
+var selectedVis = null;
+
+//Variáveis de dados
+
+var entireData = null;
+var cf = null
+var gradesDimension = null;
+var numStudents = null;
+
+
+//Seletores e callbacks
+
+//Funções
+function drawMapInitial()
+{
+    myMap = new L.Map("leafletmap", {center: [-10.3333,-53.2], zoom: 4}).addLayer(new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"));
+}
+
+//Função que faz a legenda
+//Cria uma série de pequenos retângulos com as cores da escala, para representar a escala.
+var addLegend = function(target, transformer, width)
+{
+    textMargin = 30
+    leftMargin = 15
+    //Eu usei let aqui porque eu vi um exemplo de coisas dando errado com var quando você usa múltiplas cópias de uma mesma função de certas formas.
+    //Além do tamanho dos retângulos, deixamos uma margem para o texto.
+    let svg = target.append("svg").attr("width",width + leftMargin + 30).attr("height",20 + textMargin + 5).attr("id","legend")
+
+    let squareWidth = 5
+
+    let [lb,ub] = transformer.domain()
+
+    //Uma escala para converter a posição do retângulo no svg para uma cor da escala.
+    let posScaler = d3.scaleLinear().domain([0,width]).range(transformer.domain())
+
+    //Adicionando o texto com os limites superiores e inferiores da escala.
+    svg.append("text").attr("x",0).attr("y",15).text(lb)
+    svg.append("text").attr("x",width - squareWidth).attr("y",15).text(ub)
+
+	for(let pos = 0; pos < width; pos += squareWidth)
+	{
+		v = posScaler(pos)
+
+		svg.append("rect").attr("x",leftMargin + pos).attr("y",textMargin+5).attr("width",squareWidth).attr("height",20).attr("fill",transformer(v)).attr("v",v).attr("fill-opacity",0.3)
+	}
+}
+
+var callbackSelectVis = function(event)
+{ 
+    let radioValue = document.querySelector('input[name="w"]:checked').value;
+    
+    let de = document.querySelector('select#dropdown')
+    let dropdownValue = de.options[de.selectedIndex].value;    
+     
+    let vis = radioValue + '-' + dropdownValue
+    selectedVis = dropdownValue
+    
+    console.log(vis)
+    
+    d3.select("svg#legend").remove()
+   	
+   	if(geoLayer != null)
+   	{
+   	    myMap.removeLayer(geoLayer)
+   	}
+	
+    
+    if(radioValue == "vis1")
+    {
+        cb = drawStatesVis
+    }
+    else if(radioValue == "vis2")
+    {
+        cb = drawInstitutionsVis
+    }
+
+    fillTextDiv(vis)
+
+    if(JSONData == null)
+    {
+        d3.json('project_files/brazil-states.geojson',cb)
+    }
+    else
+    {
+        cb(null, null)
+    }
+}
+
+
+
+var formElement = document.querySelector('form#selectvis')
+formElement.addEventListener('change', callbackSelectVis);
+
+var dropdownElement = document.querySelector('select#dropdown')
+dropdownElement.addEventListener('change', callbackSelectVis);
+
+
+
+drawMapInitial()
+
